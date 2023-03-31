3ede364b2699ada70b848d21baf8faf967ce653f
caoyuchun <335003032@qq.com>
2023-03-31 15:02:31 +0800

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "人工智能曹夏瑶",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "人工智能曹夏瑶",
     statusBarStyle: "black-translucent",
   },
   themeColor: "#fafafa",
